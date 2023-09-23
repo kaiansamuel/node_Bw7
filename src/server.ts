818816b1e74b18155358c4190ddb66078adc68fa
Kaian Samuel <kaiansamuelster@gmail.com>
2023-09-23 18:33:27 -0300

    Pagina 404

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import mainRoutes from './routes/index'
 import painelRoutes from './routes/painel'
 
@@ -6,5 +6,8 @@ const server = express()
 
 server.use('/', mainRoutes)
 server.use('/painel', painelRoutes)
+server.use((req: Request, res: Response) => {
+  res.status(404).send('Pagina não encontrada!')
+})
 
 server.listen(3040)
